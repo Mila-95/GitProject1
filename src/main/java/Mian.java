9a630162c4001de583e4098b49b89db17dcade0e
Mila-95 <mila.kushchak@gmail.com>
2022-05-14 12:03:34 -0400

    2nd commit

--- src/main/java/Mian.java
+++ src/main/java/Mian.java
@@ -1,4 +1,9 @@
 package main.java;
 
 public class Mian {
+    public static void main(String[] args) {
+        System.out.println("I got");
+        System.out.println("I am Groot2");
+        System.out.println("I am Groot12");
+    }
 }
